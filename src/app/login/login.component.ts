900893090525d1bbc63c272bf93a733742a9efc5
Jasneet <jasneetpalsingh619@gmail.com>
2024-01-12 18:40:41 -0700

    added method to handle jwt tokens payload data, decode it, and store it in sessionStorage, then navigate to the netflix home page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 declare var google:any;
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private router = inject(Router);
   ngOnInit(): void {
     google.accounts.id.initialize({
         client_id: '583699743152-7744uv4hj2e1ko74a2nm2s2mrfgj9idh.apps.googleusercontent.com',
         callback: (resp: any) => {
-
+          this.handleLogin(resp)
         }
       });
 
@@ -21,5 +23,20 @@ export class LoginComponent implements OnInit {
       shape: 'rectangle',
       width: 350
     })
+  }
+
+  private decodeToken(token: string){
+    return JSON.parse(atob(token.split(".")[1]));
+  }
+
+  handleLogin(response: any){
+    if(response){
+      //decoding the token
+      const payload = this.decodeToken(response.credential);
+      //store in session
+      sessionStorage.setItem("loggedInUser", JSON.stringify(payload));
+      //navigate to home
+      this.router.navigate(['home'])
+    }
   }
 }
