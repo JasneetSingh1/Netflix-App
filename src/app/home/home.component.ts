0867b339daa7a949e9d952969a3043a1abfb88c6
Jasneet <jasneetpalsingh619@gmail.com>
2024-01-12 18:56:27 -0700

    created a sign-out service using google.accounts.id.disableAutoSelect() method for the home page to the log in page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  auth = inject (AuthService);
 
+  signOut(){
+    this.auth.signOut();
+  }
 }
