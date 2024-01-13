50c7cbe19b5051cd3ffc8ac3ca5efdc24ea6e25e
Jasneet <jasneetpalsingh619@gmail.com>
2024-01-12 18:24:31 -0700

    integrated Google JavaScript Sign-in API, used OAuth to create client id to be used in the javascript sign in integration for emails

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+declare var google:any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+        client_id: '583699743152-7744uv4hj2e1ko74a2nm2s2mrfgj9idh.apps.googleusercontent.com',
+        callback: (resp: any) => {
 
+        }
+      });
+
+      google.accounts.id.renderButton(document.getElementById("google-btn"), {
+      theme: 'filled_blue',
+      size: 'large',
+      shape: 'rectangle',
+      width: 350
+    })
+  }
 }
