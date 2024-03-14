6d009aece3c61bf6344efad6bbabb90e896e987d
Jasneet <jasneetpalsingh619@gmail.com>
2024-03-14 10:48:08 -0600

    added navigation to diff pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { TvshowsComponent } from './pages/tvshows/tvshows.component';
+import { RecentlyAddedComponent } from './pages/recently-added/recently-added.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: 'tv-shows', component: TvshowsComponent},
+  {path: 'recently-added', component: RecentlyAddedComponent}
 ];
 
 @NgModule({
