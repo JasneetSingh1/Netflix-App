51b6d8ab6d1460fe9de038472ef64398e3944bd8
Jasneet <jasneetpalsingh619@gmail.com>
2024-03-13 18:09:52 -0600

    initialized the swiper

--- src/app/shared/component/movie-slider/movie-slider.component.ts
+++ src/app/shared/component/movie-slider/movie-slider.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-movie-slider',
   templateUrl: './movie-slider.component.html',
   styleUrls: ['./movie-slider.component.css']
 })
-export class MovieSliderComponent {
+export class MovieSliderComponent implements OnInit {
+  
+  @ViewChild('swiperContainer') swiperCOntainer!: ElementRef
+ 
+ 
+  ngOnInit(): void {
+    
+  }
+
+  private inItSwiper(){
+    return new Swiper(this.swiperCOntainer.nativeElement,{
+      slidesPerView: 3,
+      slidesPerGroup: 2,
+      centeredSlides: true,
+      loop: true,
+      breakpoints: {
+        600: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        900: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        1200: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1500: {
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1800: {
+          slidesPerView: 5,
+          slidesPerGroup: 6,
+          spaceBetween: 5,
+          centeredSlides: false,
+        }
+      }
+    })
+  }
 
 }
